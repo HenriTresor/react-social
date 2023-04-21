801322d749b11b510a2d16a37d0bba1ecd8046e7
henritresor <shimwamanahenritresor@gmail.com>
2023-04-21 10:08:19 +0200

    added mongo_uri

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connection = mongoose.connect('mongodb://127.0.0.1:27017/react-social', {
+export const connection = mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
